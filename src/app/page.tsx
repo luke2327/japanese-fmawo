3a491219fdbc4f12eeb04e93ccdf086f8be03a86
luke2327 <qwepoi8526@gmail.com>
2023-12-22 19:09:28 +0900

    add: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,44 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        별난 한일커플 変わってる日韓カップル ｡·͜·｡
+        Welcome to my Application
       </h1>
-      <div className="prose prose-neutral dark:prose-invert flex items-center gap-2">
+      <div className="prose-neutral dark:prose-invert flex items-start gap-2">
         <div id="profile">
           <Image
-            src="/profile.jpeg"
+            src="/profile.svg"
             width={120}
             height={48}
             alt="profile image"
-            className="rounded-[1rem]"
+            className="rounded-[1rem] border border-neutral-100"
           />
         </div>
-        <div>
-          <span className="dark:text-white">
-            흔하지만 흔하지 않은 별난 <b>한일커플</b> 입니다.
-          </span>
-          <br />
-          IT개발자와 IT대학에 다니고있는 간사이 지역 여자친구와의 별난 일상을
-          적는 블로그 입니다.
+        <div className="flex flex-col gap-2">
+          <h4 className="dark:text-white text-lg">Luke2327</h4>
+          <p className="text-neutral-400">
+            Product-focused Fullstack Developer
+          </p>
         </div>
+      </div>
+      <div id="description" className="mt-4">
+        <h5 className="text-lg">About</h5>
+        <p className="text-neutral-400">
+          GM, Im Luke2327. I enjoy building dynamic, creative products from
+          start to finish. Focused on developing intuitive experiences that
+          constantly grow and improve based on user metrics. Always shipping.
+        </p>
+      </div>
+      <div id="resume-link" className="mt-4">
+        <h5 className="text-lg">Resume</h5>
+        <Link href="https://resume.maplew.com/">
+          <p className="text-neutral-400 transition-colors hover:text-neutral-200">
+            https://resume.maplew.com
+          </p>
+        </Link>
       </div>
     </section>
   );
