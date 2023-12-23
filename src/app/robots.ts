88af0e2ad3c6679b995742649f4110d2aa00b91c
luke2327 <qwepoi8526@gmail.com>
2023-12-23 10:05:42 +0900

    mod: robots txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
+        allow: "*",
       },
     ],
     sitemap: `${config.host}/sitemap.xml`,
