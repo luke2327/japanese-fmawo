5a08d347a996c38bb96f9405f4355ee118a3f5e2
luke2327 <qwepoi8526@gmail.com>
2023-12-30 19:29:14 +0900

    Add new UI components for form handling

--- src/app/proverb/regist/page.tsx
+++ src/app/proverb/regist/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
@@ -21,74 +21,62 @@ import {
   getPostings,
   translate,
 } from "@/app/db/blog-client";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { PostingInsert } from "@/interface/blog.interface";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { PutBlobResult } from "@vercel/blob";
+import * as z from "zod";
+import { Label } from "@/components/ui/label";
+
+const formSchema = z.object({
+  titleKo: z.string(),
+  titleJa: z.string(),
+  titleEn: z.string(),
+  writer: z.string(),
+  publishedAt: z.date(),
+  contents: z.string(),
+});
 
 const RegistrationPage: React.FC = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const [titleKo, setTitleKo] = useState("");
-  const [titleEn, setTitleEn] = useState("");
-  const [titleJa, setTitleJa] = useState("");
-  const [writer, setWriter] = useState("Maria");
-  const [contents, setContents] = useState("");
-  const [useParse, setUseParse] = useState(false);
-  const [publishedAt, setPublishedAt] = useState<Date>();
-  const [postings, setPostings] = useState<
-    Awaited<ReturnType<typeof getPostings>>
-  >([]);
-  const { toast } = useToast();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      titleKo: "",
+      titleJa: "",
+      titleEn: "",
+      writer: "Maria",
+      publishedAt: new Date(),
+      contents: "",
+    },
+  });
 
-  const fetch = async () => {
-    const postings = await getPostings();
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!inputFileRef.current?.files) {
+      throw new Error("No file selected");
+    }
 
-    setPostings(postings);
-  };
+    const file = inputFileRef.current.files[0];
 
-  useEffect(() => {
-    fetch();
-  }, []);
+    const response = await fetch(
+      `/api/proverb/upload-thumbnail?filename=${file.name}`,
+      {
+        method: "POST",
+        body: file,
+      }
+    );
 
-  const handleTitleKoreanChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitleKo(e.target.value);
-  };
+    const { url: thumbnailUrl } = (await response.json()) as PutBlobResult;
 
-  const handleTitleEnglishChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitleEn(e.target.value);
-  };
-
-  const handleTitleJapaneseChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setTitleJa(e.target.value);
-  };
-
-  const handleWriterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWriter(e.target.value);
-  };
-
-  const handleContentsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setContents(e.target.value);
-  };
-
-  const handleUseParseChange = (e: boolean) => {
-    setUseParse(e);
-  };
-
-  const translateTitleEnglish = async () => {
-    const translatedText = await translate({
-      text: titleJa,
-      target: "en",
-      source: "ja",
-    });
-
-    setTitleEn(translatedText);
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    setSubmitted(true);
-
-    const slug = titleEn
+    const slug = values.titleEn
       .toLowerCase()
       .replace(/,/g, " ")
       .replace(/\s\s/g, " ")
@@ -98,162 +86,247 @@ const RegistrationPage: React.FC = () => {
 
     try {
       mdx = getFormattedMDX({
-        contents,
+        contents: values.contents,
         slug,
         title: {
-          titleEnglish: titleEn,
-          titleJapanese: titleJa,
-          titleKorean: titleKo,
+          titleEnglish: values.titleEn,
+          titleJapanese: values.titleJa,
+          titleKorean: values.titleKo,
         },
+        thumbnailUrl,
       }) as string;
     } catch (e) {
       console.error(e);
     }
 
     const payload: PostingInsert = {
-      titleKo,
-      titleEn,
-      titleJa,
-      writer,
+      titleKo: values.titleKo,
+      titleEn: values.titleEn,
+      titleJa: values.titleJa,
+      writer: values.writer,
       contents: mdx,
-      publishedAt: format(publishedAt as Date, "yyyy-MM-dd HH:mm:SS"),
+      publishedAt: format(values.publishedAt as Date, "yyyy-MM-dd HH:mm:SS"),
       slug,
       postIndex: postings.length + 1,
-      thumbnailUrl: `/images/${slug}/picture.jpg`,
+      thumbnailUrl,
     };
 
     await addPosting(payload);
 
+    // 리셋
+    form.reset();
+    inputFileRef.current.files = null;
+    inputFileRef.current.value = "";
+
+    // 등록완료 토스트 메세지
     toast({
       title: "登録完了",
-      description: titleJa,
+      description: values.titleJa,
       duration: 3000,
     });
 
-    fetch();
-
+    // 블로그 포스팅 정보 불러오기
+    init();
+    // 버튼 로딩 비활성화
     setSubmitted(false);
-    setTitleKo("");
-    setTitleEn("");
-    setTitleJa("");
-    setWriter("Maria");
-    setContents("");
-    setPublishedAt(undefined);
-    setUseParse(false);
+  }
+
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
+  const [submitted, setSubmitted] = useState(false);
+  const [useParse, setUseParse] = useState(false);
+  const [postings, setPostings] = useState<
+    Awaited<ReturnType<typeof getPostings>>
+  >([]);
+  const { toast } = useToast();
+
+  const init = async () => {
+    const postings = await getPostings();
+
+    setPostings(postings);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleUseParseChange = (e: boolean) => {
+    setUseParse(e);
+  };
+
+  const translateTitleEnglish = async () => {
+    const translatedText = await translate({
+      text: form.getValues("titleJa"),
+      target: "en",
+      source: "ja",
+    });
+
+    form.setValue("titleEn", translatedText);
   };
 
   return (
     <section className="font-raleway">
       <h1 className="mb-2 text-lg">新しい投稿（{postings.length + 1}番目）</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="titleKorean">タイトル (韓国語)</label>
-        <Input
-          type="text"
-          id="titleKorean"
-          value={titleKo}
-          onChange={handleTitleKoreanChange}
-          className="mt-1 mb-4"
-        />
-        <div>
-          <label htmlFor="titleJapanese">タイトル (日本語)</label>
-          <Input
-            type="text"
-            id="titleJapanese"
-            value={titleJa}
-            onChange={handleTitleJapaneseChange}
-            className="mt-1 mb-4"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col justify-center gap-4">
+          <FormField
+            control={form.control}
+            name="titleKo"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>タイトル (韓国語)</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription className="dark:text-neutral-400">
+                  例：「한국의 속담」
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <div>
-          <div className="flex items-center gap-2">
-            <label htmlFor="titleEnglish">タイトル (英語)</label>
-            <Button
-              type="button"
-              className="py-0 text-[12px] h-5 px-2"
-              onClick={translateTitleEnglish}>
-              翻訳
+          <FormField
+            control={form.control}
+            name="titleJa"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>タイトル (日本語)</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription className="dark:text-neutral-400">
+                  例：「韓国のことわざ」
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="titleEn"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex items-center gap-2">
+                  タイトル (英語)
+                  <Button
+                    type="button"
+                    className="py-0 text-[12px] h-5 px-2"
+                    onClick={translateTitleEnglish}>
+                    翻訳
+                  </Button>
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription className="dark:text-neutral-400">
+                  例：「Korean proverb」
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="writer"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>投稿者</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription className="dark:text-neutral-400">
+                  例：「Maria」
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="publishedAt"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex flex-col justify-center gap-2">
+                  <FormLabel>投稿日</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger
+                        asChild
+                        className="mt-1 mb-4 dark:bg-[#111010]">
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] justify-start text-left font-normal mb-0",
+                            !field.value && "text-muted-foreground"
+                          )}>
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                          {field.value ? (
+                            format(field.value, "yyyy-MM-dd")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          id="publishedAt"
+                          mode="single"
+                          initialFocus
+                          {...field}
+                          onSelect={field.onChange}
+                          selected={field.value}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                </div>
+
+                <FormDescription className="dark:text-neutral-400">
+                  最近投稿日 {postings[0]?.publishedAt.split(" ")[0]}
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div>
+            <Label>イメージ</Label>
+            <Input ref={inputFileRef} name="thumbnail" type="file" />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="contents"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>内容</FormLabel>
+                <FormControl>
+                  <Textarea className="mt-1 h-80" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex items-end justify-end gap-2">
+            <Checkbox
+              id="useParse"
+              checked={useParse}
+              onCheckedChange={handleUseParseChange}
+            />
+            <label
+              htmlFor="useParse"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              テンプレートを使用する
+            </label>
+            <Button className="w-20" type="submit" disabled={submitted}>
+              保存
             </Button>
           </div>
-          <Input
-            type="text"
-            id="titleEnglish"
-            value={titleEn}
-            onChange={handleTitleEnglishChange}
-            className="mt-1 mb-4"
-          />
-        </div>
-        <div>
-          <label htmlFor="writer">投稿者</label>
-          <Input
-            type="text"
-            id="writer"
-            value={writer}
-            onChange={handleWriterChange}
-            className="mt-1 mb-4"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <label htmlFor="publishedAt">投稿日</label>
-            <p className="text-neutral-400 text-[12px]">
-              最近投稿日 {postings[0]?.publishedAt.split(" ")[0]}
-            </p>
-          </div>
-          <Popover>
-            <PopoverTrigger asChild className="mt-1 mb-4 dark:bg-[#111010]">
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "w-[240px] justify-start text-left font-normal",
-                  !publishedAt && "text-muted-foreground"
-                )}>
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {publishedAt ? (
-                  format(publishedAt, "yyyy-MM-dd")
-                ) : (
-                  <span>Pick a date</span>
-                )}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                id="publishedAt"
-                mode="single"
-                selected={publishedAt}
-                onSelect={setPublishedAt}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
-
-        <div>
-          <label htmlFor="contents">内容</label>
-          <Textarea
-            id="contents"
-            value={contents}
-            onChange={handleContentsChange}
-            className="mt-1 h-80"
-          />
-        </div>
-
-        <div className="mt-2 flex items-center justify-end gap-2">
-          <Checkbox
-            id="useParse"
-            checked={useParse}
-            onCheckedChange={handleUseParseChange}
-          />
-          <label
-            htmlFor="useParse"
-            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-            テンプレートを使用する
-          </label>
-          <Button className="w-20" type="submit" disabled={submitted}>
-            保存
-          </Button>
-        </div>
-      </form>
+        </form>
+      </Form>
     </section>
   );
 };
