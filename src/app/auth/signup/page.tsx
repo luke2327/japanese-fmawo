f1b9ebe63ac372cf118cee6251241a233b6aac8d
luke2327 <qwepoi8526@gmail.com>
2024-01-03 14:44:07 +0900

    Add auth layout and login/signup pages

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,30 @@
+import { CreateAccount } from "@/components/create-account";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description:
+    "회원가입하여 세계각국의 사람들과 교류해봐요 | 会員登録して世界各国の人々と交流してみましょう | Join the Member and interact with people from all over the world",
+  openGraph: {
+    title: "Sign Up",
+    description:
+      "회원가입하여 세계각국의 사람들과 교류해봐요 | 会員登録して世界各国の人々と交流してみましょう | Join the Member and interact with people from all over the world",
+  },
+};
+
+function SignupPage() {
+  return (
+    <Card className="max-w-md w-full mx-4">
+      <CardHeader>
+        <CardTitle className="text-black dark:text-white">
+          Create an Account
+        </CardTitle>
+      </CardHeader>
+      <CreateAccount />
+    </Card>
+  );
+}
+
+export default SignupPage;
