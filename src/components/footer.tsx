8ca7ddafdd95c293e29228906771a306367b6e92
luke2327 <qwepoi8526@gmail.com>
2024-03-05 10:08:04 +0900

    src/components/logo.tsx와 src/components/footer.tsx에 변경 내용이 있습니다.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
+import Logo from "@/components/logo";
 import clsx from "clsx";
-import Image from "next/image";
 import Link from "next/link";
 
 export function Footer() {
@@ -9,7 +9,9 @@ export function Footer() {
         "mt-14 text-center font-raleway max-w-xl w-full mx-auto border-t dark:border-neutral-600"
       )}>
       <div className="flex gap-8 sm:gap-20 mt-10">
-        <div className="pt-6 relative left-2 sm:left-8">FAMWO</div>
+        <div className="relative top-6 left-2 sm:left-8">
+          <Logo />
+        </div>
         <section className="flex gap-10 text-sm mb-10">
           <div className="flex flex-col items-start gap-2">
             <h4>About</h4>
@@ -42,7 +44,7 @@ export function Footer() {
       </div>
 
       <span className="text-neutral-600 dark:text-neutral-400 border-neutral-100 text-sm text-center">
-        Copyright(C) 2024 fmawo.com All Rights Reserved.
+        Copyright(C) 2023 - 2024 fmawo.com All Rights Reserved.
       </span>
     </footer>
   );
